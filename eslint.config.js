939dfde7974087acddce0a25a39e1f1a8978d3bf
Anri Akopian <anri-akk@mail.ru>
2025-03-02 18:19:18 +0400

    added prettier, eslint config and tailwind css

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+    { ignores: ['dist', 'node_modules'] },
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: globals.browser,
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            react.configs.recommended,
+        ],
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+            indent: ['error', 4, { SwitchCase: 1 }],
+            'linebreak-style': ['error', 'unix'],
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    }
+);
